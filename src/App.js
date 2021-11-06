d6ac628c840177e3fc7128d5cb02457ce8652e17
adamaah <adamaniang1807@gmail.com>
2021-11-06 19:47:41 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class Profil extends React.Component{
+
+  //Le compteur de secondes 
+  state = {
+    seconds: 1
+  };
+
+  componentDidMount() {
+    this.timer = setInterval(() => {
+      this.setState({ seconds: this.state.seconds + 1 });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+
+  //Pour afficher - masquer les infos de profil
+  state = {
+     isActive:false,
+     
+  }
+
+  showHandler = ()=>{
+      this.setState({
+          isActive: true
+      })
+  }
+
+  hideHandler = () =>{
+      this.setState({
+          isActive: false
+      })
+  }
+
+
+  //Les infos du profil
+  state = {
+
+    fullName: "Je m'appelle Adama",
+      bio: "Je suis apprenant à Gomycode",
+      profession: "Web developer",
+      imgSrc: [
+        "/profilepic.png"
+      ]
+
+  }
+
+   render(){
+       return(
+           <div>
+           
+              {
+                this.state.isActive ?
+                <>
+                  <h1>hello {this.state.fullName}</h1> <br></br> 
+                  <h3>Un petit bio : </h3> 
+                  <p>{this.state.bio}</p> <br></br> 
+                  <h3>Profession : </h3> 
+                  <p>{this.state.profession}</p>
+                  <img src={this.state.imgSrc}></img> <br></br> 
+
+                  <p>Le compteur en secondes : {this.state.seconds}</p>
+    
+                </>
+
+                :null
+              }
+             
+              <button onClick={this.showHandler}>Afficher le profil</button>
+              <button onClick={this.hideHandler}>Masquer</button>
+           </div>
+       )
+   }
 }
 
-export default App;
+export default Profil;
